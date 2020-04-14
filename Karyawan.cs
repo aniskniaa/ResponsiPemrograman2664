bc5fcf4fc12d7df86acb81a323c6fe05a29816dc
aniskniaa <rnia578@gmail.com>
2020-04-14 11:10:20 +0700

    Responsi

---
+++ Karyawan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2664
+{
+    class Karyawan
+    {
+        public Karyawan(int nik, String nama, int gaji)
+        {
+            Nik = nik;
+            Nama = nama;
+            Gaji = gaji;
+        }
+
+        public int Nik;
+        public string Nama;
+        public int Gaji;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2664
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2664
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine(" Nim    Nama     Gaji Bulanan");
+                Console.WriteLine(" ============================");
+                Karyawan kr1 = new Karyawan(1234, "Anis", 300000);
+                Hasil(kr1);
+
+                Karyawan kr2 = new Karyawan(4987, "NIa", 200000);
+                Hasil(kr2);
+
+
+                Console.ReadLine();
+
+            }
+
+            static void Hasil(Karyawan kr)
+            {
+
+                Console.WriteLine(" {0}   {1}     {2}     ", kr.Nik, kr.Nama, kr.Gaji);
+            }
+
+
+        }
+    }
+
+
+    Console.ReadKey();
+
+        }
+    }
+}
